f572d8dd699fd6fe17c6bb677530edcd970e586b
tebanamaya1 <gnesisgroup1@gmail.com>
2022-10-24 21:26:36 -0500

    se agregan archivos for while

---
+++ loopsforyforof.js
@@ -0,0 +1,9 @@
+var estudiantes = ["maria", "sergio", "rosa", "daniel"];
+
+function saludarEstudiantes(estudiantes){
+    console.log(`hola, ${estudiante}`);
+}
+
+for(var i = 0; i < estudiantes.length; i++){
+    saludarEstudiantes(estudiantes[i]);
+}
